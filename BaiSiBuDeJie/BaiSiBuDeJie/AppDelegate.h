dca4f6d60a104a87252d2ceff5eccf5a3b118ff0
gaoshan <1328279962@qq.com>
2016-12-05 10:35:51 +0800

    创建BaiSiBuDeJie工程

---
+++ BaiSiBuDeJie/BaiSiBuDeJie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiSiBuDeJie
+//
+//  Created by most on 16/12/5.
+//  Copyright © 2016年 most. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
